72627b8099d521c2978fafb9ec923a9f38996fd1
Crotear1 <138115674+Crotear1@users.noreply.github.com>
2024-02-26 08:19:55 +0100

    Installed "huge" Framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0df37ed24cf7dea38a312be92e884601343ffb22',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0df37ed24cf7dea38a312be92e884601343ffb22',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'panique/huge' => array(
+            'pretty_version' => 'v3.3.1',
+            'version' => '3.3.1.0',
+            'reference' => 'dd96f33b68e65c5922d922cc93b907ca3b90b24e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../panique/huge',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
